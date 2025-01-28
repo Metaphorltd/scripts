a57693402adef0da2178aa0d5bb14ed22bfb0e01
Abdullah <abdullah@otaskies.com>
2025-01-28 07:48:59 +0500

    refs

--- kube.ps1
+++ kube.ps1
@@ -1,4 +1,19 @@
 iex (iwr https://raw.githubusercontent.com/metaphorltd/scripts/main/utils.ps1).Content
+function ReFetchDeployment {
+  param (
+      [Parameter(Mandatory)][string]$deployment,
+      [Parameter(Mandatory)][string]$namespace
+  )
+  
+  Write-Info "Scaling Kubernetes deployment to 0..."
+  kubectl scale deployment $deployment --replicas 0 -n $namespace
+  ThrowOnError $?
+  Start-Sleep 1
+  Write-Info "Scaling Kubernetes deployment to 1..."
+  kubectl scale deployment $deployment --replicas 1 -n $namespace
+  ThrowOnError $?
+  Start-Sleep 1
+}
 
 function CreateDeployment {
     param (
--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
 # $token = & { . ([scriptblock]::Create((iwr https://raw.githubusercontent.com/metaphorltd/scripts/main/auth.ps1).Content)); GetAuthToken -clientSecret P@kistan7861324 -scopes ShellEx }
 . ./kube.ps1
 
-CreateDeployment -kustomizePath ".\kustomize\red.portal\environments\pr" `
+CreateDeployment -kustomizePath ".\kustomize\red-portal\environments\pr" `
     -dockerImage "red.portal" `
     -app "pr47-red-portal" `
     -buildId "764" `
