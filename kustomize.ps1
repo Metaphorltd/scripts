8cf520ca58ee77f634d82d1eb7d54db99945f9cc
Abdullah <abdullah@otaskies.com>
2025-02-12 16:21:32 +0500

    refs

--- kustomize.ps1
+++ kustomize.ps1
@@ -24,9 +24,9 @@ function Invoke-TryInstallKustomize{
     $result = Get-Command kustomize -ErrorAction SilentlyContinue;
     if(!$result -or $force){
         Write-Info  "Kustomize not found. Installing..."
-        curl --silent --location --remote-name "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v3.2.3/kustomize_kustomize.v3.2.3_linux_amd64"
-        chmod a+x kustomize_kustomize.v3.2.3_linux_amd64
-        sudo mv kustomize_kustomize.v3.2.3_linux_amd64 /usr/local/bin/kustomize
+        curl --silent --location --remote-name "https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize/v5.3.0/kustomize_kustomize.v5.3.0_linux_amd64"
+        chmod a+x kustomize_kustomize.v5.3.0_linux_amd64
+        sudo mv kustomize_kustomize.v5.3.0_linux_amd64 /usr/local/bin/kustomize
         kustomize version
     }else
     {
